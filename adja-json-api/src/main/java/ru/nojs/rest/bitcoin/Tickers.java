971ab7626f1ed9c142c17a2590b3c82debb22248
vdovinds <d_vdovin@live.ru>
2015-12-16 13:55:24 +0300

    cleanup uri builder

--- adja-json-api/src/main/java/ru/nojs/rest/bitcoin/Tickers.java
+++ adja-json-api/src/main/java/ru/nojs/rest/bitcoin/Tickers.java
@@ -1,16 +1,17 @@
 package ru.nojs.rest.bitcoin;
 
-import org.codehaus.jackson.annotate.JsonAnySetter;
-import org.codehaus.jackson.annotate.JsonRawValue;
-import org.codehaus.jackson.annotate.JsonUnwrapped;
+import org.codehaus.jackson.annotate.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Tickers {
+
     Map<String, String> tickers = new HashMap<>();
 
-    @JsonAnySetter public void add(String key, String value) {
+    @JsonAnySetter
+    @JsonSetter
+    public void add(String key, String value) {
         tickers.put(key, value);
     }
     public Map<String, String> tickers() {
--- adja-json-vdovin/src/main/java/ru/vdovin/rest/bitcoin/RestEasyProxyInvocationHandler.java
+++ adja-json-vdovin/src/main/java/ru/vdovin/rest/bitcoin/RestEasyProxyInvocationHandler.java
@@ -2,6 +2,7 @@ package ru.vdovin.rest.bitcoin;
 
 import com.google.common.base.Preconditions;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -27,10 +28,12 @@ public class RestEasyProxyInvocationHandler implements InvocationHandler {
                 (method.isAnnotationPresent(GET.class) || (method.isAnnotationPresent(POST.class)))
                 , "Method must have @GET or @POST annotation");
 
-        String uri = this.uri;
+        UriComponentsBuilder uriBuilder  = UriComponentsBuilder.fromHttpUrl(this.uri);
         if (method.isAnnotationPresent(Path.class)) {
-            uri = uri + method.getAnnotation(Path.class).value();
+            uriBuilder.path(method.getAnnotation(Path.class).value());
         }
+
+        String uri = uriBuilder.build().toUriString();
 
         Map<String, String> param = new HashMap<>();
         for ( int i=0 ; i < method.getParameters().length; i++ ){
@@ -38,7 +41,6 @@ public class RestEasyProxyInvocationHandler implements InvocationHandler {
                 param.put( method.getParameters()[i].getAnnotation(PathParam.class).value(), (String)args[i]);
             }
         }
-
 
         if (method.isAnnotationPresent(POST.class)) {
             //FIXME: temporarily for test
